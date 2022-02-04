5e9df53521c1ad00b7cfbf6099df7901c8a6e46a
sagar-shivappa <shivappasagar@gmail.com>
2022-02-04 13:11:50 +0530

    sales per day fixed

--- routes/sales.js
+++ routes/sales.js
@@ -10,13 +10,31 @@ router.post("/addSales", async (req, res) => {
     let SalesHistory = await dataBase
       .collection("SalesHistory")
       .findOne({ selectedDate: req.body.selectedDate, owner: req.body.owner });
+
     if (SalesHistory) {
-      res.json({ message: "Today's Sales Available" });
+      // res.json({ message: "Today's Sales Available" });
+      console.log(SalesHistory);
+      let r = SalesHistory.sales;
+      let updateSales = await dataBase.collection("SalesHistory").updateOne(
+        { selectedDate: req.body.selectedDate, owner: req.body.owner },
+        {
+          $set: {
+            todaysSales: SalesHistory.todaysSales + req.body.todaysSales,
+          },
+        }
+      );
+      res.json({
+        message: "Today's Sales Updated",
+        updatedSales: SalesHistory.todaysSales + req.body.todaysSales,
+      });
     } else {
       let status = await dataBase
         .collection("SalesHistory")
         .insertOne(req.body);
-      res.json({ message: "Today's Sales Inserted" });
+      res.json({
+        message: "Today's Sales Inserted",
+        updatedSales: req.body.todaysSales,
+      });
     }
   } catch (error) {
     console.log(error);
