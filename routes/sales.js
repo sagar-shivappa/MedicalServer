6d6c5e99f3528e7092fad6f8d09883090bdcd44b
sagar-shivappa <shivappasagar@gmail.com>
2022-02-15 22:21:01 +0530

    Sales fixed

--- routes/sales.js
+++ routes/sales.js
@@ -1,39 +1,62 @@
 var express = require("express");
 var router = express.Router();
-const { hashing, hashCompare } = require("../library/auth");
+//const { hashing, hashCompare } = require("../library/auth");
 const { mongodb, MongoClient, dbUrl, dbName } = require("../dbConfig");
 
 router.post("/addSales", async (req, res) => {
   const client = await MongoClient.connect(dbUrl);
   try {
     let dataBase = await client.db(dbName);
-    let SalesHistory = await dataBase
-      .collection("SalesHistory")
-      .findOne({ selectedDate: req.body.selectedDate, owner: req.body.owner });
+    let SalesHistory = await dataBase.collection("SalesHistory").findOne({
+      selectedDate: req.body.selectedDate,
+      owner: req.body.owner,
+    });
 
     if (SalesHistory) {
-      // res.json({ message: "Today's Sales Available" });
-      console.log(SalesHistory);
-      let r = SalesHistory.sales;
       let updateSales = await dataBase.collection("SalesHistory").updateOne(
-        { selectedDate: req.body.selectedDate, owner: req.body.owner },
+        {
+          selectedDate: req.body.selectedDate,
+          owner: req.body.owner,
+        },
+
         {
           $set: {
-            todaysSales: SalesHistory.todaysSales + req.body.todaysSales,
+            todaysSales: SalesHistory.todaysSales + req.body.paymentMoney,
+            [`${req.body.paymentType}`]:
+              SalesHistory[`${req.body.paymentType}`] + req.body.paymentMoney,
           },
         }
       );
       res.json({
         message: "Today's Sales Updated",
-        updatedSales: SalesHistory.todaysSales + req.body.todaysSales,
+        data: await dataBase.collection("SalesHistory").findOne({
+          selectedDate: req.body.selectedDate,
+          owner: req.body.owner,
+        }),
       });
     } else {
-      let status = await dataBase
+      let response = await dataBase
         .collection("SalesHistory")
         .insertOne(req.body);
+      let updateSales = await dataBase.collection("SalesHistory").updateOne(
+        {
+          selectedDate: req.body.selectedDate,
+          owner: req.body.owner,
+        },
+
+        {
+          $set: {
+            todaysSales: req.body.paymentMoney,
+            [`${req.body.paymentType}`]: req.body.paymentMoney,
+          },
+        }
+      );
       res.json({
         message: "Today's Sales Inserted",
-        updatedSales: req.body.todaysSales,
+        res: await dataBase.collection("SalesHistory").findOne({
+          selectedDate: req.body.selectedDate,
+          owner: req.body.owner,
+        }),
       });
     }
   } catch (error) {
@@ -47,16 +70,37 @@ router.post("/getSales", async (req, res) => {
   const client = await MongoClient.connect(dbUrl);
   try {
     let dataBase = await client.db(dbName);
-    let SalesHistory = await dataBase
-      .collection("SalesHistory")
-      .find({ owner: req.body.owner })
-      .toArray();
-    res.json({ data: SalesHistory });
+    if (req.body.selectedDate == null) {
+      console.log("................1", req.body.selectedDate);
+      var findAllSales = await dataBase
+        .collection("SalesHistory")
+        .find({ owner: req.body.owner })
+        .toArray();
+    } else {
+      console.log("................", req.params.date);
+
+      var findAllSales = await dataBase.collection("SalesHistory").findOne({
+        owner: req.body.owner,
+        selectedDate: req.body.selectedDate,
+      });
+    }
+
+    //upComing Feature. To limit the dates
+    // ,selectedDate: { $gt: new Date('1940-01-01'), $lt: new Date('1960-01-01') }
+
+    // count the records
+    // var groupedSales = await dataBase
+    //   .collection("SalesHistory")
+    //   .count({ owner: req.body.owner });
+
+    res.json({ data: findAllSales });
   } catch (error) {
     console.log(error);
   } finally {
     client.close();
   }
 });
+
+// router.get("/getSales/:date", as);
 
 module.exports = router;
