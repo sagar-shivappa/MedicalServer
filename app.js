9ff876cdd9811a17da072a68c8dbf29dd0fa8019
sagar-shivappa <shivappasagar@gmail.com>
2022-02-24 19:34:34 +0530

    Stocks Added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var loginRouter = require("./routes/login");
 var SalesRouter = require("./routes/sales");
+var StocksRouter = require("./routes/stocks");
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/medical", loginRouter);
 app.use("/sales", SalesRouter);
+app.use("/stocks", StocksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
